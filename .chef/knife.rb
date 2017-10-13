1c120448ef0fa0669d597d146c65af9febfc84d1
Vadim <jsdkfj@kjdfk.com>
2017-10-13 21:17:08 +0000

    removed .chef

--- .chef/knife.rb
+++
@@ -1,9 +0,0 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
-
-current_dir = File.dirname(__FILE__)
-log_level                :info
-log_location             STDOUT
-node_name                "admin"
-client_key               "#{current_dir}/admin.pem"
-chef_server_url          "https://ec2-34-233-133-106.compute-1.amazonaws.com/organizations/cheftest"
-cookbook_path            ["#{current_dir}/../cookbooks"]
